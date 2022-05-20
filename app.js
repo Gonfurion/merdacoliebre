cae3b2583c8fe9d4f6b200feec76715b3118f22e
Gonfurion <gorza86@gmail.com>
2022-05-19 22:55:44 -0300

    clase5 sin css

--- app.js
+++ app.js
@@ -1,51 +1,38 @@
-const express = require("express")
+const express = require('express');
 const app = express();
-const path = require('path');
+app.use(express.static('public'));
 
-app.listen(3000, () => console.log("server con puerto 3000"))
 
-app.use( express.static( 'public' ))
-app.use( express.static( 'views' ))
+app.listen(3000, ()=>{
+    console.log('Servidor funcionando');
+});
 
-app.use(express.static(path.join(__dirname, "./public")) )
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname + '/views/index.html');
+});
 
-app.get("/", (req, res) => {
-    res.sendFile (path.join (__dirname, "./views/index.html"))
-})
+app.get('/register', (req,res)=>{
+    res.sendFile(__dirname + '/views/register.html');
+});
 
-app.get("/ofertas", (req, res) => {
-    res.sendFile (path.join (__dirname, "./ofertas.html"))
-})
+app.get('/ingresa', (req,res)=>{
+    res.sendFile(__dirname + '/views/ingresa.html');
+});
 
-app.get("/ayuda", (req, res) => {
-    res.sendFile (path.join (__dirname, "./views/ayuda.html"))
-})
+app.get('/ayuda', (req,res)=>{
+    res.sendFile(__dirname + '/views/ayuda.html');
+});
 
-app.get("/creacuenta", (req, res) => {
-    res.sendFile (path.join (__dirname, "./views/creacuenta.html"))
-})
+app.get('/misCompras', (req,res)=>{
+    res.sendFile(__dirname + '/views/misCompras.html');
+});
 
-app.get("/ingresa", (req, res) => {
-    res.sendFile (path.join (__dirname, "./views/ingresa.html"))
-})
-
-app.get("/misCompras", (req, res) => {
-    res.sendFile (path.join (__dirname, "./views/misCompras.html"))
-})
-
-app.get("/register", (req, res) => {
-    res.sendFile (path.join (__dirname, "./views/register.html"))
-})
-
-app.get("/tiendasOficiales", (req, res) => {
-    res.sendFile (path.join (__dirname, "./views/tiendasOficiales.html"))
-})
-
-app.get("/vender", (req, res) => {
-    res.sendFile (path.join (__dirname, "./views/vender.html"))
-})
-
-// app.get("/", (req, res) => {
-//     res.sendFile (path.join (__dirname, "./views/.html"))
-// })
-
+app.get('/ofertas', (req,res)=>{
+    res.sendFile(__dirname + '/views/ofertas.html');
+});
+app.get('/vender', (req,res)=>{
+    res.sendFile(__dirname + '/views/vender.html');
+});
+app.get('/login', (req,res)=>{
+    res.sendFile(__dirname + '/views/login.html');
+});
