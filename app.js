8911a90a7941485555645b2d5616757ea8533afc
Gonfurion <gorza86@gmail.com>
2022-05-19 21:09:39 -0300

    update clase 3

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express")
 const app = express();
 const path = require('path');
 
-app.listen(3000, () => console.log("server con puerto 3030"))
+app.listen(3000, () => console.log("server con puerto 3000"))
 
 app.use( express.static( 'public' ))
 app.use( express.static( 'views' ))
